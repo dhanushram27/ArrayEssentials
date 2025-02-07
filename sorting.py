ed41085b257e9b3be709fe3ec0fdccfebd678f23
Dhanushram <127968736+dhanushram27@users.noreply.github.com>
2025-02-07 23:39:02 +0530

    Create sorting.py

---
+++ sorting.py
@@ -0,0 +1,7 @@
+def sort_numbers(numbers):
+    numbers.sort()
+    return numbers
+    
+numbers = [10, 11, 12, 13, 14, 19, 18, 17, 16, 15]
+sorted_asc = sort_numbers(numbers)
+print("Sorted :", sorted_asc)
